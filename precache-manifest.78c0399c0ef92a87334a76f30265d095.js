cebfe78e5243c25111603991032a5453044b94c6
iHelin <ihelin@outlook.com>
2019-09-25 07:12:50 +0000

    Deploying to gh-pages from master 318c925732872f0d3a6fe2d15d63af4462419f42

---
+++ precache-manifest.78c0399c0ef92a87334a76f30265d095.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baa64dd76de25bd9c59e86fe8cd3cbfc",
+    "url": "/github-actions-demo/index.html"
+  },
+  {
+    "revision": "9bfaa0ec863da26d5a78",
+    "url": "/github-actions-demo/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "548455f8974ba42b3c5e",
+    "url": "/github-actions-demo/static/js/2.c195622b.chunk.js"
+  },
+  {
+    "revision": "9bfaa0ec863da26d5a78",
+    "url": "/github-actions-demo/static/js/main.e0eaa008.chunk.js"
+  },
+  {
+    "revision": "998659acbca3dc355780",
+    "url": "/github-actions-demo/static/js/runtime-main.1195ea6c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/github-actions-demo/static/media/logo.25bf045c.svg"
+  }
+]);
